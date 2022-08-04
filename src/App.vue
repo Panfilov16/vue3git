bc509aed5bba21cf1bf7ca87be1f951661f52c58
Aleksander Panfilov <panfilov.a.e@mail.ru>
2022-08-04 20:58:13 +0300

    Работаем

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,27 @@
 <template>
-<div class="post">
-  <div><strong>Название:</strong> Пост о JS</div>
-  <div><strong>Описание:</strong> JS круть</div>
-</div>
-<div class="post">
-  <div><strong>Название:</strong> Пост о JS</div>
-  <div><strong>Описание:</strong> JS круть</div>
-</div>
-<div class="post">
-  <div><strong>Название:</strong> Пост о JS</div>
-  <div><strong>Описание:</strong> JS круть</div>
+<form class="input">
+  <input type="text" placeholder="Название">
+  <input type="text" placeholder="Описание">
+  <button class="btn">Создать</button>
+</form>
+<div class="post" v-for="post in posts">
+<div><strong>Название:</strong> {{post.title}}</div>
+<div><strong>Описание:</strong> {{post.body}}</div>
 </div>
 </template>
 
 <script>
-
+export default {
+ data() {
+  return {
+    posts: [
+      {id: 1, title: "JavaScript 1", body: "Описание 1"},
+      {id: 2, title: "JavaScript 2", body: "Описание 2"},
+      {id: 3, title: "JavaScript 3", body: "Описание 3"},
+    ]
+  }
+ },
+}
  
 </script>
 
@@ -26,8 +33,18 @@
 }
 .post {
   padding: 15px;
-  border: 2px solid teal
+  border: 2px solid teal;
+  margin-top: 15px;
 }
+.btn {
+  margin: 10px;
+  padding: 10px;
+  border: 2px solid teal;
+  background: none;
+  margin-top: 15px;
+}
+.input {
 
+}
 
 </style>
