68731c84516f7afb232e37e9ac90f3ed3e94cdfb
Aleksander Panfilov <panfilov.a.e@mail.ru>
2022-08-04 20:39:56 +0300

    vue3git start

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,33 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+<div class="post">
+  <div><strong>Название:</strong> Пост о JS</div>
+  <div><strong>Описание:</strong> JS круть</div>
+</div>
+<div class="post">
+  <div><strong>Название:</strong> Пост о JS</div>
+  <div><strong>Описание:</strong> JS круть</div>
+</div>
+<div class="post">
+  <div><strong>Название:</strong> Пост о JS</div>
+  <div><strong>Описание:</strong> JS круть</div>
+</div>
 </template>
 
+<script>
+
+ 
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+    margin: o;
+    padding: 0;
+    box-sizing: border-box;
+}
+.post {
+  padding: 15px;
+  border: 2px solid teal
 }
 
-nav {
-  padding: 30px;
-}
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
